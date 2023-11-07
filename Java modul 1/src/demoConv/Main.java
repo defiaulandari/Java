63f950cdf1982c204241ef59ae41ad8670f72201
defiaulandari <defiaulandari18@gmail.com>
2023-11-07 14:31:52 +0700

    Age

---
+++ Java modul 1/src/demoConv/Main.java
@@ -0,0 +1,18 @@
+package demoConv;
+
+public class Main {
+    public static void main(String[] args) {
+        convCM m_to_cm = new convCM();
+        m_to_cm.convMtoCM();
+
+        convMM m_to_mm = new convMM();
+        m_to_mm.convMtoCM();
+
+        dispConv all = new dispConv();
+        all.Display();
+
+    }
+
+}
+
+
